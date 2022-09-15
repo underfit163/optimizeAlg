5a2ce51e1d0eb6b5a246cd5446ce7f4dedae6df9
underfit163 <163tyschckun163@gmail.com>
2022-09-15 13:37:33 +0400

    optimizeAlg1

---
+++ src/main/java/Main.java
@@ -0,0 +1,92 @@
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = new int[1_000_000];
+        int[] arr2 = new int[1_000_000];
+        Random random = new Random();
+        System.out.println("Start");
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = random.nextInt();
+        }
+        System.out.println("Arr1 has been initialized");
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = random.nextInt();
+        }
+        System.out.println("Arr2 has been initialized");
+        long t1 = System.currentTimeMillis();
+        int[] result1 = getNewMas(arr1, arr2);
+        long t2 = System.currentTimeMillis();
+        System.out.println((t2 - t1) + ", " + result1.length);
+        t1 = System.currentTimeMillis();
+        int[] result2 = getNewMasMySet(arr1, arr2);
+        t2 = System.currentTimeMillis();
+        System.out.println((t2 - t1) + ", " + result2.length);
+
+
+
+//        MySet<Integer> mySet = new MySet<>(arr2.length);
+//        for (int i = 0; i < arr2.length; i++) {
+//            mySet.addVal(arr2[i]);
+//        }
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.print(mySet.getVal(i) + " | ");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < arr2.length; i++) {
+//            System.out.print(mySet.getIndex(i) + " | ");
+//        }
+//        System.out.println();
+//        System.out.println(mySet.contains(17));
+    }
+
+    //Задание: Меняем реализацию на MySet<T> (класс) реализовать максимально эффективно
+    public static int[] getNewMas(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length) {
+            return getNewMas(arr2, arr1);
+        }
+        Set<Integer> set1 = new HashSet<>(arr1.length);//Замена на MySet
+
+        int[] a = new int[arr2.length];
+
+        for (int j : arr1) {
+            set1.add(j);
+        }
+        int k = 0;
+        for (int i : arr2) {
+            if (set1.contains(i)) {
+                a[k] = i;
+                k++;
+            }
+        }
+        return Arrays.copyOf(a, k);
+    }
+
+    //Задание: Меняем реализацию на MySet<T> (класс) реализовать максимально эффективно
+    public static int[] getNewMasMySet(int[] arr1, int[] arr2) {
+        if (arr1.length > arr2.length) {
+            return getNewMasMySet(arr2, arr1);
+        }
+        MySet<Integer> mySet = new MySet<>(arr1.length);
+
+        int[] a = new int[arr2.length];
+
+        for (int j : arr1) {
+            mySet.addVal(j);
+        }
+        int k = 0;
+        for (int i : arr2) {
+            if (mySet.contains(i)) {
+                a[k] = i;
+                k++;
+            }
+        }
+        return Arrays.copyOf(a, k);
+    }
+}
---
+++ src/main/java/MySet.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class MySet<T> {
+    private final T[] values;
+    private final int[] indexes;
+    private int i = 0;
+
+    public MySet(int n) {
+        values = (T[]) new Object[n];
+        indexes = new int[n];
+        Arrays.fill(indexes, -1);
+    }
+
+    public T getVal(int i) {
+        return values[i];
+    }
+
+    public int getIndex(int i) {
+        return indexes[i];
+    }
+
+    public void addVal(T val) {
+        int idx = Math.abs((Integer) val) % values.length;
+        addRecursive(val, idx);
+    }
+
+    private void addRecursive(T val, int idx) {
+        if (values[idx] == null) {
+            values[idx] = val;
+        } else if (!values[idx].equals(val)) {
+            if (indexes[idx] == -1) {
+                while (values[i] != null) {
+                    i += 1;
+                }
+                values[i] = val;
+                indexes[idx] = i;
+            } else {
+                addRecursive(val, indexes[idx]);
+            }
+        }
+    }
+
+    public boolean contains(T val) {
+        int idx = Math.abs((Integer) val) % values.length;
+        return containsRecursive(val, idx);
+    }
+
+    private boolean containsRecursive(T val, int idx) {
+        if (values[idx] != null) {
+            if (values[idx].equals(val)) {
+                return true;
+            } else if (indexes[idx] != -1) {
+                return containsRecursive(val, indexes[idx]);
+            }
+        }
+        return false;
+    }
+}
